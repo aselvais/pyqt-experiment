2343feaf7aaaeb5e8850c5578d5b46ab6d5a55f4
Selvais, Arnaud <arnaud.selvais@deluxe.com>
2020-09-09 15:26:06 -0400

    minor changes

--- libs/ui/MainWindow.py
+++ libs/ui/MainWindow.py
@@ -1,49 +1,58 @@
+"""
+For tabular data check https://www.learnpyqt.com/courses/model-views/qtableview-modelviews-numpy-pandas/
+for tech talk project :)
+
+"""
 from PyQt5 import QtWidgets
 from PyQt5 import uic
+from PyQt5.QtWidgets import QPushButton, QCommandLinkButton, QTableView, QColumnView, QPlainTextEdit
+from typeguard import typechecked
 
 
 class MainWindow(QtWidgets.QMainWindow):
-    """
 
-    Args:
-        QtWidgets ([type]): [description]
-    """
-
-    # ui -> QtWidgets.QPlainTextEdit
+    ui: QtWidgets
 
     def __init__(self, *args, **kwargs):
-        """
-        """
         super().__init__(*args, **kwargs)
         self.ui = uic.loadUi("ui/mainwindow.ui", self)
         self._setup_connectors()
 
+    def _get_btn_clickme(self) -> QPushButton:
+        return self.ui.btn_clickme
+
+    def _get_btn_linkme(self) -> QCommandLinkButton:
+        return self.ui.btn_linkme
+
+    def _get_table_view(self) -> QTableView:
+        return self.ui.table_view
+
+    def _get_column_view(self) -> QColumnView:
+        return self.ui.column_view
+
+    def _get_plain_text_edit(self) -> QPlainTextEdit:
+        return self.ui.plain_text_edit
+
     def _setup_connectors(self):
-        """
-        """
-        self.ui.btn_clickme.clicked.connect(self.clicked_clickme)
-        self.ui.btn_linkme.clicked.connect(self.clicked_linkme)
+        self._get_btn_clickme().clicked.connect(self.clicked_clickme)
+        self._get_btn_linkme().clicked.connect(self.clicked_linkme)
 
     def clicked_clickme(self):
-        """
-        """
-        v = "button btn_linkme clicked !!!"
-        print(v)
-        self._log(v)
+        self._log("button btn_linkme clicked !!!")
 
     def clicked_linkme(self):
-        """
-        """
-        v = "button btn_linkme clicked !!!! YEAH!"
-        print(v)
-        self._log(v)
+        self._log("button btn_linkme clicked !!!! YEAH!")
 
-    def _log(self, txt=""):
+    def _log(self, txt: str = ""):
         """Add text to the plain text box in the GUI
 
         Args:
             txt (str, optional): [description]. Defaults to "".
         """
-        self.ui.plain_text_edit.appendPlainText(txt)
+        self._get_plain_text_edit().appendPlainText(txt)
 
-
+    def add_table_data(self):
+        # setModel(self, model: QtCore.QAbstractItemModel) -> None: ...
+        tbl = self._get_table_view()
+        tbl.setModel()
+        
