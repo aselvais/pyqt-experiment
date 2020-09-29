6efbb20d935c45de2da4c4f74fa7c35e37cf81c6
Selvais, Arnaud <arnaud.selvais@deluxe.com>
2020-09-29 11:01:33 -0400

    add background image test

--- libs/ui/MainWindow.py
+++ libs/ui/MainWindow.py
@@ -5,10 +5,11 @@ for tech talk project :)
 """
 # from typeguard import typechecked
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QPushButton, QCommandLinkButton, QTableView, QColumnView, QPlainTextEdit
+from PyQt5.QtGui import QImage
+from PyQt5.QtWidgets import QGraphicsView, QProgressBar, QCommandLinkButton, QTableView, QPlainTextEdit
 from libs.logmanagement.Analyzer import Analyzer
 from libs.ui.TableModel import TableModel
-
+import time
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -19,17 +20,28 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__(*args, **kwargs)
         self.ui = uic.loadUi("ui/mainwindow.ui", self)
         self._setup_connectors()
+        self._get_progress_bar().setValue(0)
+        view: QGraphicsView = self._get_graphics_view()
+        view.setStyleSheet("background-image: url(./assets/images/backgroundtile.jpg)")
         # text_edit_message
         # text_edit_funcname
         # text_edit_process
         # combo_box_levelname
         # date_edit
+        # graphics_view
+        # progress_bar
 
     def _setup_connectors(self):
         self._get_btn_submit().clicked.connect(self.clicked_btn_submit)
 
     def _get_btn_submit(self) -> QCommandLinkButton:
         return self.ui.btn_submit
+
+    def _get_graphics_view(self) -> QGraphicsView:
+        return self.ui.graphics_view
+    
+    def _get_progress_bar(self) -> QProgressBar:
+        return self.ui.progress_bar
 
     def _get_table_view(self) -> QTableView:
         return self.ui.table_view
@@ -42,6 +54,7 @@ class MainWindow(QtWidgets.QMainWindow):
         when clicking the linkme button
         """
         self._log("button btn_submit clicked !!!! YEAH!")
+        self._get_progress_bar().setValue(50)
         self.add_table_data()
 
     def _log(self, txt: str = ""):
@@ -58,6 +71,10 @@ class MainWindow(QtWidgets.QMainWindow):
         Push data to the data grid
         """
         self._get_table_view().setModel(self.get_data())
+        self._get_progress_bar().setValue(100)
+        time.sleep(1)
+        self._get_progress_bar().setValue(0)
+
 
     def get_data(self):
         """
